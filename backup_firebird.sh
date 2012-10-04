020aed15f3d46f8dd45b601e1ac2c55d91e64a8d
Jeronimo Zucco <jczucco@gmail.com>
2012-10-04 09:57:54 -0300

    - Alterado caminho do security database (referencia: http://www.destructor.de/firebird/gbak.htm) - Alterado variaveis para não incluir a "/" no fim - Mudado o caminho padrão do firebird de acordo com o pacote rpm binário
    Signed-off-by: Jeronimo Zucco <jczucco@gmail.com>

--- backup_firebird.sh
+++ backup_firebird.sh
@@ -2,9 +2,9 @@
 USER=sysdba
 PASS="xxxxx"
 HOST=localhost
-FBHOME=/usr/lib/firebird/
-DATADIR=/storage/firebird/
-BACKUPDIR=/storage/backup/firebird/
+FBHOME="/opt/firebird"
+DATADIR="/storage/firebird"
+BACKUPDIR="/storage/backup/firebird"
 
 echo "$(date) Inicio Backup Firebird"
 [ -d ${BACKUPDIR} ] && {
@@ -16,16 +16,16 @@ echo "$(date) Inicio Backup Firebird"
         # backup das bases de dados
 	cd ${DATADIR}
 	ls *.FDB *.GDB *.fdb *.gdb 2>/dev/null |while read DB; do	
-                echo "Executando backup de ${DATADIR}${DB}"
-                /usr/bin/gbak -t -user ${USER} -password "${PASS}" ${HOST}:${DATADIR}${DB} ${BACKUPDIR}${DB} || exit 2
-		echo "Compactando ${BACKUPDIR}${DB}"
-		gzip ${BACKUPDIR}${DB}		
+                echo "Executando backup de ${DATADIR}/${DB}"
+                /usr/bin/gbak -t -user ${USER} -password "${PASS}" ${HOST}:${DATADIR}/${DB} ${BACKUPDIR}/${DB} || exit 2
+		echo "Compactando ${BACKUPDIR}/${DB}"
+		gzip ${BACKUPDIR}/${DB}		
         done
 
-        echo "Executando backup de ${FBHOME}security2.fdb"
-        /usr/bin/gbak -t -user ${USER} -password "${PASS}" -se ${HOST}:service_mgr sec ${BACKUPDIR}security2.fdb || exit 2
-	echo "Compactando ${BACKUPDIR}security2.fdb"
-	gzip ${BACKUPDIR}security2.fdb			
+        echo "Executando backup de ${FBHOME}/security2.fdb"
+        /usr/bin/gbak -t -user ${USER} -password "${PASS}" -se ${HOST}:service_mgr ${FBHOME}/security2.fdb ${BACKUPDIR}/security2.fbk || exit 2
+	echo "Compactando ${BACKUPDIR}/security2.fdb"
+	gzip ${BACKUPDIR}/security2.fdb			
 echo "FIM   $(date)"
 } || {
         echo "${BACKUPDIR} não existe!"
